8e0b5588f1ee3c3ccd642b697c72ef47414e364f
unknown <you@example.com>
2022-10-07 21:21:59 +0800

    PHP Exercises

---
+++ PHP Exercise 2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>PHP exercise 2</title>
+</head>
+<body>
+<?php
+$x=10;
+$y= 7;
+echo "$x + $y = " . ($x + $y) ."<br>";
+echo "$x - $y = " . ($x - $y) ."<br>";
+echo "$x * $y = " . ($x * $y) ."<br>";
+echo "$x / $y = " . ($x / $y) ."<br>";
+echo "$x % $y = " . ($x % $y) ."<br>";
+?>
+</body>
+</html>
---
+++ PHP Exercise 3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>PHP exercise 3</title>
+</head>
+<body>
+<?php
+$x=8;
+echo "Value is now " . $x . "<br>"; $x += 2;
+echo "Add 2. Value is now " . $x . "<br>"; $x -= 4;
+echo "Subtract 4. Value is now " . $x . "<br>"; $x *= 5;
+echo "Multiply by 5. Value is now " . $x . "<br>"; $x /= 3;
+echo "Divide by 3. Value is now " . $x . "<br>"; $x = ++$x;
+echo "Increment value by one. Value is now " . $x . "<br>"; $x = --$x;
+echo "Decrement value by one. Value is now" . $x . "<br>";
+?>
+</body>
+</html>
